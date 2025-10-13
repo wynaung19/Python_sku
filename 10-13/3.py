b0c2df85373b046aa3c45e1be5ad8ab895913bdf
Wai Yan Naung <waiyannaung19@gmail.com>
2025-10-13 16:14:38 +0900

    if else with loop

--- 10-13/3.py
+++ 10-13/3.py
@@ -1,13 +1,26 @@
-a = int(input("Score : "))
-if a > 100:
-    print("Stupid")
-elif a >= 90:
-    print("A")
-elif a >= 80:
-    print("B")
-elif a >= 70:
-    print("C")
-elif a < 70:
-    print("F")
-else:
-    print("Stupid")
+scores = []
+name,score,grade = '',0,'A'
+while True:
+    
+    score = int(input("Score : "))
+
+    if score > 100 or score < 0:
+        print("END")
+        break
+    
+    name = input("Name : ")
+    if score >= 90:
+        grade = 'A'
+    elif score >= 80:
+        grade = 'B'
+    elif score >= 70:
+        grade = 'C'
+    elif score >= 60:
+        grade = 'D'
+    else:
+        grade = 'F'
+
+    scores.append([name,score,grade])
+
+for i in scores:
+    print(i[0],":",i[1],":",i[2])
