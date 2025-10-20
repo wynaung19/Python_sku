0ea24cf2829769882cdd66fcf90edd42014a1c19
Wai Yan Naung <60054637+wynaung19@users.noreply.github.com>
2025-10-20 15:26:42 +0900

    10-20

--- 10-20/4.py
+++ 10-20/4.py
@@ -1,9 +1,10 @@
 i,num = 0,0
 
-#num = int(input("Dan for : "))
-
 for i in range(1,11):
     for j in range(1,11):
-        print("%dX%2d=%3d" % (j,i,j*i),end=" | ")
+        if(j >9):
+            print("%d x %2d = %3d" % (j,i,j*i),end=" | ")
+        else:
+            print("%d x %2d =%3d" % (j,i,j*i),end=" | ")
     print("\n")
 
