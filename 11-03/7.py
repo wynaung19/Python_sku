abf10fe8790bf9b9dc34e3af95e10368c5a9b2bd
Wai Yan Naung <60054637+wynaung19@users.noreply.github.com>
2025-11-03 16:26:10 +0900

    11-3

--- 11-03/7.py
+++ 11-03/7.py
@@ -1,11 +1,25 @@
-a=[10,20,30,40,50,60,70,80,90]
-
-a[1]=200
-
-a[2:3] = [300,301]
-
-a[3]=[300,1]
-
-del(a[8])
-
+a = [20,10,30]
 print(a)
+a.append(40)
+print("append 40" ,a)
+print(a.pop())
+print(a)
+
+a.sort()
+print("sort",a)
+
+a.reverse()
+print("rev",a)
+
+print("index 20 : ", a.index(20))
+
+a.insert(2,222)
+print("insert(2,222)",a)
+
+a.remove(222)
+print("rem(222)",a)
+
+a.extend([77,88,77])
+print("extend([77,88,77])",a)
+
+print("77 count :",a.count(77))
