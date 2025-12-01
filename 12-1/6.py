b7122f0b36e2b4dd7f77dfeb79eba3124bafd4fa
Wai Yan Naung <60054637+wynaung19@users.noreply.github.com>
2025-12-01 16:19:33 +0900

    12-1

--- 12-1/6.py
+++ 12-1/6.py
@@ -3,9 +3,26 @@ from tkinter.filedialog import *
 window = Tk()
 window.geometry("400x400")
 
-file = askopenfilename(parent = window,filetypes = (("GIF files","*.gif"),("All Files","*.*")))
+mainMenu = Menu(window)
+window.config(menu = mainMenu)
 
-photo = PhotoImage(file = file)
-pLabel = Label(window, image = photo)
-pLabel.place(x=0,y=0)
+def fun_open():
+    filepath = askopenfilename(parent = window,filetypes = (("GIF files","*.gif"),("All Files","*.*")))
+    photo = PhotoImage(file = filepath)
+    pLabel.configure(image = photo)
+    pLabel.image = photo
+def fun_exit():
+    window.quit()
+    window.destroy()
+
+photo = PhotoImage()
+pLabel = Label(window,image = photo)
+pLabel.pack(expand = 1, anchor = CENTER)
+
+fileMenu = Menu(mainMenu,tearoff = 0)
+mainMenu.add_cascade(label = "File",menu = fileMenu)
+fileMenu.add_command(label = "Open",command = fun_open)
+fileMenu.add_separator()
+fileMenu.add_command(label = "Exit",command = fun_exit)
+
 window.mainloop()
